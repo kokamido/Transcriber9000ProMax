61ba4618220c20a86a7ec5e39df33e226e1a4f3f
kokamido <kokamidokokosian@gmail.com>
2026-05-13 03:16:49 +0000

    package structure

---
+++ src/transcriber9000/transcriber_models/gigaam.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING, Callable
+
+from gigaam import load_audio, load_model, GigaAM, GigaAMASR, GigaAMEmo  # pyright: ignore[reportMissingTypeStubs]
+
+from transcriber9000.dto import TranscriberModel
+
+
+if TYPE_CHECKING:
+    from torch import Tensor
+
+
+def load_gigaam(model: TranscriberModel) -> GigaAM | GigaAMEmo | GigaAMASR:
+    match model:
+        case TranscriberModel.GIGAAM_V3_CTC:
+            return load_model("v3_ctc")
+        case TranscriberModel.GIGAAM_V3_RNNT:
+            return load_model("v3_rnnt")
+        case TranscriberModel.GIGAAM_V3_E2E_CTC:
+            return load_model("v3_e2e_ctc")
+        case TranscriberModel.GIGAAM_V3_E2E_RNNT:
+            return load_model("v3_e2e_rnnt")
+        case _:
+            raise ValueError(f"Can't load {model=}")
+
+
+def load_audio_gigaam(sample_rate: int) -> Callable[[str], Tensor]:
+    return lambda audio_path: load_audio(audio_path=audio_path, sample_rate=sample_rate)
